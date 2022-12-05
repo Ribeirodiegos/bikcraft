b7b8567b6e94c323eb8a9762dc8d053f4b694fd4
Ribeirodiegos <santos.diegoribeiro@gmail.com>
2022-12-05 12:24:41 -0300

    Mais ajustes
    Otimizando o site com Javascript, galerias e perguntas.

--- js/script.js
+++ js/script.js
@@ -24,3 +24,44 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// PEGUNTAS FREQUENTES
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", "true");
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
+
+// GALERIA DE BICICLETA
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  console.log(media);
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
